1b4db1a5641ebf630c1a7f560bec0a44b00dbde9
octophi <joey.li@duke.edu>
2018-07-03 13:48:14 -0400

    integrated matlab code

--- circuit_builder.py
+++ circuit_builder.py
@@ -1,18 +1,19 @@
 from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister, QISKitError
 
-def buildCirc(qc, qr, circuitArray, perm):
+def buildCirc(qc, qr, cr, circuitArray, perm):
     for gate in circuitArray:
-        addGate(qc, qr, gate, perm)
-
+        lastGate = addGate(qc, qr, gate, perm)
+    qc.measure(qr,cr)
 
 def addGate(qc, qr, gate, perm):
     gateSet = {
-        "x": addX,
-        "z": addZ,
-        "h": addH,
-        "s": addS,
-        "cx": addCX,
-        "cz": addCZ,
+        "X": addX,
+        "Z": addZ,
+        "H": addH,
+        "P": addS,
+        "CNOT": addCX,
+        "CZ": addCZ,
+        "Permute": permute
     }
     
     # convert gate string to gate array
@@ -24,6 +25,10 @@ def addGate(qc, qr, gate, perm):
         print("Some nonsense was ignored")
     else:
         addGens(qc, qr, gateSpec[1:],perm)
+    if gateSpec[0] == "Permute":
+        return 1
+    else:
+        return 0
     
 def addX(qc, qr, gateNums,perm):
     for num in gateNums:
@@ -55,3 +60,7 @@ def addCZ(qc, qr, gateNums,perm):
     qub2 = perm[int(gateNums[1])]
     qc.cz(qr[qub1], qr[qub2])
 
+def permute(qc, qr, gateOrder,perm):
+    permCopy = perm.copy()
+    for i in range(len(perm)):
+        perm[i+1] = permCopy[int(gateOrder[i])]
